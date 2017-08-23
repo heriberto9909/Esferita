a5525d3a84ff8e961eed19eed6d11faaf243f335
Heriberto Perez <Heriberto Perez>
2017-08-23 17:37:54 -0500

    Esfera

---
+++ Assets/Scripts/LaserScript.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LaserScript : MonoBehaviour {
+
+    public float mFireRate = .5f;
+    public float mFireRange = 50f;
+    public float mHitForce = 100f;
+    public int mLaserDamage = 100;
+
+    private LineRenderer mLaserLine;
+
+    private bool mLaserLineEnabled;
+
+    private WaitForSeconds mLaserDuration = new WaitForSeconds(0.05f);
+
+    private float mNextFire;
+
+    private AudioSource laser;
+
+    int contador = 0;
+
+    public Text puntuacion;
+
+    void Start()
+    {
+        mLaserLine = GetComponent<LineRenderer>();
+        laser = GetComponent<AudioSource>();
+    }
+
+    private void Fire()
+    {
+        Transform cam = Camera.main.transform;
+
+        mNextFire = Time.time + mFireRate;
+
+        Vector3 rayOrigin = cam.position;
+
+        mLaserLine.SetPosition(0, transform.up * -10f);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(rayOrigin, cam.forward, out hit, mFireRange))
+        {
+            mLaserLine.SetPosition(1, hit.point);
+
+            CubeBehaviorScript cubeCtr = hit.collider.GetComponent<CubeBehaviorScript>();
+            CubeBehaviorScript2 cubeCtr2 = hit.collider.GetComponent<CubeBehaviorScript2>();
+            if (cubeCtr != null)
+            {
+                if (hit.rigidbody != null)
+                {
+                    contador = contador + 5;
+                    Debug.Log("Tiro");
+                    Debug.Log("Contador " + contador);
+                    puntuacion.text = "Marcador: " + contador;
+                    hit.rigidbody.AddForce(-hit.normal * mHitForce);
+                    cubeCtr.Hit(mLaserDamage);
+                }
+            }
+
+            if (cubeCtr2 !=null)
+            {
+                if(hit.rigidbody != null)
+                {
+                    Debug.Log("Incorrecto");
+                    hit.rigidbody.AddForce(-hit.normal * mHitForce);
+                    cubeCtr2.Hit(mLaserDamage);
+                }
+            }
+
+        }
+        else
+        {
+            mLaserLine.SetPosition(1, cam.forward * mFireRange);
+        }
+
+        StartCoroutine(LaserFx());
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > mNextFire)
+        {
+            Fire();
+            laser.Play();
+        }
+    }
+
+    private IEnumerator LaserFx()
+    {
+        mLaserLine.enabled = true;
+
+
+        yield return mLaserDuration;
+        mLaserLine.enabled = false;
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour {
+
+    public GameObject mCubeObj;
+
+    public int mTotalCubes = 10;
+
+    public float mTimeToSpawn = 1f;
+
+    private GameObject[] mCubes;
+
+    private bool mPositionSet;
+
+    private bool SetPosition()
+    {
+        
+        Transform cam = Camera.main.transform;
+
+       
+        transform.position = cam.forward * 10;
+        return true;
+    }
+
+    private IEnumerator SpawnLoop()
+    {
+        StartCoroutine(ChangePosition());
+
+        yield return new WaitForSeconds(0.2f);
+
+        int i = 0;
+        while (i <= (mTotalCubes - 1))
+        {
+
+            mCubes[i] = SpawnElement();
+            i++;
+            yield return new WaitForSeconds(Random.Range(mTimeToSpawn, mTimeToSpawn * 3));
+        }
+    }
+
+    private GameObject SpawnElement()
+    {
+        GameObject cube = Instantiate(mCubeObj, (Random.insideUnitSphere * 4) + transform.position, transform.rotation) as GameObject;
+        float scale = Random.Range(0.5f, 2f);
+        cube.transform.localScale = new Vector3(scale, scale, scale);
+        return cube;
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnLoop());
+
+        mCubes = new GameObject[mTotalCubes];
+    }
+
+  
+    private IEnumerator ChangePosition()
+    {
+
+        yield return new WaitForSeconds(0.2f);
+        
+        if (!mPositionSet)
+        {
+            
+            if (VuforiaBehaviour.Instance.enabled)
+                SetPosition();
+        }
+    }
+}
